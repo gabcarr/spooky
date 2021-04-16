7694b1ed007891ce75b6bfaa2dc07ab8e807da5f
gabcarr <81604266+gabcarr@users.noreply.github.com>
2021-04-16 10:32:02 -0700

    Add files via upload

---
+++ app.R
@@ -0,0 +1,76 @@
+#' ---
+#' title: "Intro to Shiny"
+#' author: "Gabriela Carr"
+#' date: '`r format(Sys.Date(), "%B %d, %Y")`'
+#' output:
+#'    html_document:
+#'       number_sections: true
+#'       toc: true
+#'       toc_float:
+#'          collapsed: true
+
+#' Load in libraries
+library(shiny)
+library(shinythemes)
+library(tidyverse)
+
+#' Run an example
+runExample("01_hello")
+
+#' Load in Halloween data
+spooky <- read.csv("spooky_data.csv") # Could also use read_csv (tidyverse version)
+
+#' Create a blank user interface:
+ui <- fluidPage()
+
+#' Create the user interface for app:
+ui <- fluidPage(
+  titlePanel("I am adding a title!"),
+  sidebarLayout(
+    sidebarPanel("put my widgets here",
+                 selectInput(inputId = "state_select",
+                             label = "Choose a state",
+                             choices = unique(spooky$state)),
+                 radioButtons(inputId = "region_select",
+                              label = "Choose region:",
+                              choices = unique(spooky$region_us_census))),
+    mainPanel("put my outputs here",
+              p("State's top candies:"),
+              tableOutput(outputId = "candy_table"),
+              p("Region's top costumes:"),
+              plotOutput(outputId = "costume_graph")
+    )))
+
+#' Create a blank server function:
+server <- function(input, output) {}
+
+#' Subsetting dataset to just what user wants (on server side now):
+server <- function(input, output) {
+  state_candy <- reactive({ # Reactive object: input from user interface
+    spooky %>%
+      filter(state == input$state_select) %>% # Interactive objects need to be
+      # wrapped in reactive function
+      select(candy, pounds_candy_sold)})
+      output$candy_table <- renderTable({
+      state_candy()})
+  region_costume <- reactive({
+    spooky %>%
+      filter(region_us_census == input$region_select) %>%
+      count(costume, rank)
+  })
+  
+  
+  output$costume_graph <- renderPlot({
+    ggplot(region_costume(), aes(x = costume, y = n)) +
+      geom_col(aes(fill = rank)) +
+      coord_flip() +
+      scale_fill_manual(values = c("black","purple","orange")) +
+      theme_minimal()
+  })
+}
+
+
+
+#' Combine them into an app:
+shinyApp(ui = ui, server = server)
+
